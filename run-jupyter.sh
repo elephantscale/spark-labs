24b5cad7cdeebb0536fa7fb1be84b650b1330af7
Sujee Maniyam <sujee@elephantscale.com>
2022-03-17 23:18:59 -0700

    updated setup instructions
    - moved to spark 3.2.1
    - cleaned up some instructions

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -5,16 +5,6 @@
 ## leave this line commented out (do not over-ride the default path)
 
 #export PATH=$HOME/anaconda3/bin:$PATH
-export SPARK_HOME=$HOME/apps/spark
+export SPARK_HOME=$HOME/spark
 jupyter lab
 
-## --- older approach ---
-# export PYSPARK_PYTHON=$(which python3)
-# export PYSPARK_DRIVER_PYTHON=$(which jupyter)
-# export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
-
-## pyspark i local mode
-# $SPARK_HOME/bin/pyspark
-
-## pyspark connect to cluster
-#$SPARK_HOME/bin/pyspark --master spark://localhost:7077  --executor-memory 4g --driver-memory 1g
